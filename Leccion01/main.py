222f2794ff37cd3f510891e54f01ed9c50138392
Cantisano Fabian Alejandro <cantisanofabi@gmail.com>
2022-06-27 12:35:49 -0300

    curso python leccion01

---
+++ Leccion01/main.py
@@ -0,0 +1,31 @@
+# enviar un saludo a la consola
+print("Hola mundo")
+
+#variables
+miVariable = "Hola mundo desde python"
+print(miVariable)
+print(miVariable)
+
+miVariable = 10
+print(miVariable)
+print(miVariable)
+print("*****************")
+x = 10
+y = 2
+z = x + y
+print("suma de", x, "mas", y, "es: ", z)
+print("*****************")
+#Tipos de datos
+x = 10
+print(type(x))
+y = "hola"
+print(type(y))
+z = True
+print(type(z))
+print("*****************")
+#cadena (string)
+banda = "Aerosmith"
+comentario = "the best rock band"
+print("Mi grupo favorito es: " + banda + " " + comentario)
+print
+
